aca893b74cc25d7bba33fd928d4ffd4f7fa29903
Konstantinos Anastasakis <kanastasakis26@massmutual.com>
2020-03-10 11:45:26 -0400

    Added Plotly to the app.

---
+++ client/ts/plots/index.ts
@@ -0,0 +1,30 @@
+import {heatmap} from "./heatmap";
+
+/**
+ * Selects plot create function by plot type
+ * @param {string} type
+ * @param data
+ */
+export function createPlot(type:string, data:any, chartHolderId:string) {
+    switch(type) {
+        case "DD":
+            createHeatMap(data, chartHolderId);
+            break;
+        case "DC":
+        case "CD":
+            // TODO: Add plot function
+            break;
+        case "CC":
+            // TODO: Add plot function
+            break;
+    }
+}
+
+function createHeatMap(data:any, chartHolderId:string) {
+    let keys = Object.keys(data);
+    let xAxisTitle:string = keys[0];
+    let yAxisTitle:string = keys[1];
+    let xvals = Object.values(data[keys[0]]);
+    let yvals = Object.values(data[keys[1]]);
+    heatmap(xvals, yvals, xAxisTitle, yAxisTitle,chartHolderId);
+}
