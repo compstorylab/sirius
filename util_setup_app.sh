6d8a1fd1f732a9dbb532f9d2469df1c64204cef2
Konstantinos Anastasakis <kanastasakis26@massmutual.com>
2020-04-27 10:54:51 -0400

    Changes to the setup script. Added a remove script.

--- util_setup_app.sh
+++ util_setup_app.sh
@@ -23,26 +23,33 @@ command_exists "conda"
 command_exists "git"
 
 # Create the env using conda
-echo_step "Create the virtual environment"
+echo_step "Create the conda environment"
 conda create -y -n sirius_env python=3.7 pip
 
 # Start it up
-echo_step "Activate the virtual environment"
+echo_step "Activate the conda environment"
 source /anaconda3/etc/profile.d/conda.sh
 conda activate sirius_env
 
 # Add conda packages
-echo_step "Add Pip the virtual environment"
+echo_step "Add Pip to the conda environment."
 conda install -y -n sirius_env pip
+echo_step "Add Plotly to conda environment."
 conda install -y -n sirius_env -c plotly plotly-orca
 
 # Add Pip packages
+echo_step "INstall requirements using Pip."
 pip install -r requirements.txt
+echo_step "Install sirius as a package."
 pip install -e .
 
 # Create and/or append to the environment file
+echo_step "Create the env file."
 echo "SIRIUS_SETTINGS_SECRET_KEY=$(openssl rand -base64 66)" >> .env
 
 # Setup database for Django
+echo_step "Setup the database for Django."
 python manage.py migrate
 
+echo_step "Start the conda environment"
+echo "Run the following command: 'conda activate sirius_env'"
